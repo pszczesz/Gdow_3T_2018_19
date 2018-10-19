ce2c7ad78fe8254aad368fbb6850e3c084b42cbd
prof <prof@Pracownia12>
2018-10-19 11:14:34 +0200

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

---
+++ cw4/cw4.js
@@ -0,0 +1,19 @@
+
+window.onload = start;
+
+function start(){
+    let h1 = document.querySelector("h1");
+    h1.style.color = "yellow";
+    let pars = document.querySelectorAll("p");
+    //pars[2].style.backgroundColor = "red";
+    for(let i=0; i<pars.length;i++){
+        pars[i].onclick = klik;
+    }
+}
+function klik(){
+    if(this.style.backgroundColor == "red"){
+        this.style.backgroundColor = "white";
+    }else{
+        this.style.backgroundColor = "red";
+    }
+}
