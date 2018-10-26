ecff316b4f06d234872f5c31aecc183dc9907ea2
prof <prof@Pracownia12>
2018-10-26 11:44:07 +0200

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- cw6/cw6.js
+++ cw6/cw6.js
@@ -4,4 +4,27 @@ function start(){
     document.querySelector("#gener").onclick = klik;
 }
 
-
+function klik(){
+    let cols = parseInt(document.querySelector("#cols").value);
+    let rows = parseInt(document.querySelector("#rows").value);
+    if(isNaN(cols) || isNaN(rows)){
+        document.querySelector("#wynik").innerHTML = "Błedne dane";
+    }else{
+        if(cols<=0 || cols>50) cols=20;
+        if(rows<=0 || rows>50) rows=20;
+        document.querySelector("#wynik").innerHTML = generTab(rows,cols);
+    }
+}
+function generTab(rows,cols){
+    let html = "<table>";
+    for(let i=1;i<=rows;i++){
+        html += "<tr>";
+        for(let j=1; j<=cols;j++){
+            let wyr = i==1 || j==1 ? "class='first'" :""
+            html += "<td "+wyr+">"+(i*j)+"</td>";
+        }
+        html +="</tr>";
+    }
+    html += "</table>";
+    return html;
+}
