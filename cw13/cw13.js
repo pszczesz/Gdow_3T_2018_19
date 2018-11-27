326d633da77435fbe7bcc0de9f12a8c5d5026311
prof <prof@Pracownia12>
2018-11-27 12:17:04 +0100

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- cw13/cw13.js
+++ cw13/cw13.js
@@ -1,5 +1,7 @@
 window.onload = start;
 function start(){
+    let dane = getWorkers();
+    document.querySelector("#tabelka").innerHTML = genTab(dane);
     let trs = document.querySelectorAll("tr");
     for(let i=0;i<trs.length;i++){
         trs[i].oncontextmenu = function (event){
@@ -8,6 +10,28 @@ function start(){
         };
         trs[i].onmousedown = klik;        
     }
+}
+function genTab(dane){
+    let html = "<table>";
+    html += "<tr><th>Lp</th><th>Imię</th><th>Nazwisko</th><th>Pensja</th></tr>";
+    let lp =0;
+    for(let i=0;i<dane.length;i++){
+        html += "<tr>";      
+        html += "<td>"+(++lp)+"</td>";   
+        for(let j=0;j<dane[i].length;j++){           
+           html += "<td>"+dane[i][j]+"</td>"; 
+        }
+        html += "</tr>"
+    }
+    return html +"</table>";
+}
+function getWorkers(){
+    return [["Anna","Nowak",2700]
+        ,["Mariusz","Gryk",33300]
+        ,["Edward","Kowal",6900]
+        ,["Monika","Truk",5500]
+        ,["Ryszard","Hajduk",4500]
+        ,["Dominik","Fralek",4100]];
 }
 function klik(event){
     switch(event.button){
