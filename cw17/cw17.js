6dd8cf50e56d527d7db353a6360188ba688960ab
prof <prof@Pracownia12>
2018-12-11 12:14:27 +0100

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- cw17/cw17.js
+++ cw17/cw17.js
@@ -1,3 +1,16 @@
 
+window.onload = start;
 
+function start(){
+    setInterval(zmiana,3000);    
+}
+function zmiana(){
+    let licz1 = Math.floor(Math.random()*255);
+    let licz2 = Math.floor(Math.random()*255);
+    let licz3 = Math.floor(Math.random()*255);
+    document.querySelector("#napis").style.color = "rgb("+licz1+","+licz2+","+licz3+")";
+    let oldObrazek = document.querySelector("#obrazek").getAttribute("src");
+    let obrazek = oldObrazek === "ob1.png" ? "ob2.png" :"ob1.png";
+    document.querySelector("#obrazek").setAttribute("src",obrazek);
+}
 
