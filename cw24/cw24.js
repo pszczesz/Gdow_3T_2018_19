f3dd0324c27ccda0b7bf25d7e10d64931e78a58a
prof <prof@Pracownia12>
2019-02-05 11:44:42 +0100

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- cw24/cw24.js
+++ cw24/cw24.js
@@ -1,16 +1,17 @@
 window.onload = start;
 
-function start(){
+function start() {
     document.querySelector("#add").onclick = add;
+    setOnClick();
 }
-function add(){
+function add() {
     let imie = document.querySelector("#imie");
     let nazwisko = document.querySelector("#nazwisko");
     let kwota = document.querySelector("#kwota");
     let imOK = Validate(imie);
     let naOK = Validate(nazwisko);
     let kwOK = ValidateKwota(kwota);
-    if(imOK && naOK && kwOK){
+    if (imOK && naOK && kwOK) {
         let tab = document.querySelector("#tab");
         let row = tab.insertRow(-1);
         let cellImie = row.insertCell(0);
@@ -21,31 +22,49 @@ function add(){
         cellNazwisko.innerHTML = nazwisko.value.trim();
         cellKwota.innerHTML = kwota.value;
         cellOp.innerHTML = "<input type='button' value='Usuń'/>";
-        let btns = document.querySelectorAll("td input");
-       // console.log(btns);
-        for(let i=0;i<btns.length;i++){
-            btns[i].onclick = function (){
-              console.log(this.parentNode.parentNode);  
-            };
-        }
+        setOnClick();
     }
 }
-function Validate(elem){
+function setOnClick() {
+    let btns = document.querySelectorAll("td input");
+    let rows = document.querySelector("#tab").rows;
+    for(let i=0;i<rows.length;i++){
+        rows[i].onmousedown = function (){
+            console.log("Nacisnieto przycisk");
+            this.style.fontWeight = "bold";
+        };
+        rows[i].onmouseup = function (){
+            console.log("Zwolniono przycisk");
+            this.style.fontWeight = "normal";
+        };
+    }
+    // console.log(btns);
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].onclick = function () {            
+            let tr = this.parentNode.parentNode;
+            let tabelka = document.querySelector("#tab");
+            console.log(tabelka);
+            console.log(tr.rowIndex);
+            tabelka.deleteRow(tr.rowIndex);            
+        };
+    }
+}
+function Validate(elem) {
     let next = elem.nextElementSibling;
-    if(elem.value.trim()!=''){
+    if (elem.value.trim() != '') {
         next.innerHTML = "";
         return true;
-    }else{
+    } else {
         next.innerHTML = "Błedne dane!!!";
         return false;
     }
 }
-function ValidateKwota(elem){
+function ValidateKwota(elem) {
     let next = elem.nextElementSibling;
-    if(!isNaN(parseFloat(elem.value))){
+    if (!isNaN(parseFloat(elem.value))) {
         next.innerHTML = "";
         return true;
-    }else{
+    } else {
         next.innerHTML = "Błedne dane!!!";
         return false;
     }
