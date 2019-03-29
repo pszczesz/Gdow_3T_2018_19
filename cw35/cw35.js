f012fafcfd153211f1fe3975b029a7a2b23d4283
prof <prof@Pracownia12>
2019-03-29 08:59:48 +0100

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- cw35/cw35.js
+++ cw35/cw35.js
@@ -1,12 +1,15 @@
 window.onload = function (){
     document.querySelector("#oblicz").onclick = function (){
        let metr = 4000;
-       let pokoj = 1000;
-       let lazienka = 2000;
-       let metraz = parseFloat(document.querySelector("#metraz").value);
-       let pokoje = parseInt(document.querySelector("#pokoje").value);
-       let kafelki = document.querySelector("#kafelki").checked ? lazienka : 0;
-       document.querySelector("#wynik").innerHTML = "Koszt mieszkania: "
+       let pokoj = 1000;       
+       let metraz = parseFloat(document
+               .querySelector("#metraz").value);
+       let pokoje = parseInt(document
+               .querySelector("#pokoje").value);
+       let kafelki = document.querySelector("#kafelki")
+               .checked ? 2000 : 0;
+       document.querySelector("#wynik")
+               .innerHTML = "Koszt mieszkania: "
                     +(metr*metraz+pokoj*pokoje+kafelki);
     };
 };
