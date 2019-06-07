606207a775a9a803af80f2fc20d65bdc7a455a32
prof <prof@Pracownia12>
2019-06-07 11:24:42 +0200

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

---
+++ cw39/cw39.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+   $("#start").click(function () {
+       console.log($("#object").position().left);
+       if($("#object").position().left<1000){
+           $("#object").animate({left: '80%'},1000);
+       }else{
+           $("#object").animate({left: '0px'},5000);
+       }
+        
+    }); 
+    $("#panel").click(function () {
+        let width = $(document).width();
+        console.log(width);
+        console.log($("#panel").position().left);
+       if($("#panel").position().left>width-100){
+          $("#panel").animate({right: '10px'}); 
+       }else{
+           $("#panel").animate({right: '-240px'});
+       } 
+    });
+});
+
+
