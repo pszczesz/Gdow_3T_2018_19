2d6cbdf75682bb724d8944de64fb8ea7460f53e3
prof <prof@Pracownia12>
2019-06-07 12:20:34 +0200

    Merge origin/master
    Conflicts:
    	cw1/cw1.html
    	index.html

--- cw39/cw39.js
+++ cw39/cw39.js
@@ -5,13 +5,10 @@ $(document).ready(function () {
            $("#object").animate({left: '80%'},1000);
        }else{
            $("#object").animate({left: '0px'},5000);
-       }
-        
+       }        
     }); 
     $("#panel").click(function () {
-        let width = $(document).width();
-        console.log(width);
-        console.log($("#panel").position().left);
+        let width = $(document).width();      
        if($("#panel").position().left>width-100){
           $("#panel").animate({right: '10px'}); 
        }else{
